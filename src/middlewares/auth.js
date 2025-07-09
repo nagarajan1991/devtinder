b914b8ff45043f44fe746bbc9d9a8a12fb0bd64f
nagarajan1991 <nagarajan1991@gmail.com>
2025-07-09 22:54:27 +0100

    APIs updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,43 +1,29 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const auth = async (req, res, next) => {
-    try {
-        // Get token from cookie or header
-        const token = req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
-        
-        if (!token) {
-            return res.status(401).json({ error: 'Please authenticate.' });
-        }
-
-        if (!process.env.JWT_SECRET) {
-            return res.status(500).json({ error: 'Server configuration error.' });
-        }
-
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        // Find user by id
-        const user = await User.findById(decoded._id);
-        
-        if (!user) {
-            return res.status(401).json({ error: 'Please authenticate.' });
-        }
-
-        // Attach user to request object
-        req.user = user;
-        req.token = token;
-        
-        next();
-    } catch (error) {
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({ error: 'Invalid token.' });
-        }
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ error: 'Token expired.' });
-        }
-        res.status(500).json({ error: 'Authentication error.' });
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not valid!!!!!!!!!");
     }
+
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 };
 
-module.exports = auth; 
+module.exports = {
+  userAuth,
+};
