13048106381ceddf73cedb9e8c08fb16f197cd22
nagarajan1991 <nagarajan1991@gmail.com>
2025-07-12 23:18:16 +0100

    Feed and pagintion API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,17 @@ authRouter.post("/signup", async (req, res) => {
     // Validation of data
     validateSignUpData(req);
 
-    const { firstName, lastName, emailId, password } = req.body;
+    const { 
+      firstName, 
+      lastName, 
+      emailId, 
+      password, 
+      age, 
+      gender, 
+      photoUrl, 
+      about, 
+      skills 
+    } = req.body;
 
     // Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
@@ -22,10 +32,34 @@ authRouter.post("/signup", async (req, res) => {
       lastName,
       emailId,
       password: passwordHash,
+      age,
+      gender,
+      photoUrl,
+      about,
+      skills
     });
 
     await user.save();
-    res.send("User Added successfully!");
+    
+    // Return the created user (excluding password)
+    const userResponse = {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailId: user.emailId,
+      age: user.age,
+      gender: user.gender,
+      photoUrl: user.photoUrl,
+      about: user.about,
+      skills: user.skills,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
+    
+    res.status(201).json({
+      message: "User Added successfully!",
+      user: userResponse
+    });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
