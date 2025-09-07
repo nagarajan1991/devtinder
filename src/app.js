964a637b2a53b450b5570ad9dd4a7bf69cd8ccfe
nagarajan1991 <nagarajan1991@gmail.com>
2025-09-07 19:14:26 +0100

    namastedev final

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require('dotenv').config();
+const http = require("http");
 
-app.use(cors({
-  origin: "http://localhost:5173", // or your frontend URL
-  credentials: true
-}));
+require("dotenv").config();
+
+require("./utils/cronjob");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -17,18 +23,24 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is successfully listening on port ${process.env.PORT}...`);
+    server.listen(process.env.PORT, () => {
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
